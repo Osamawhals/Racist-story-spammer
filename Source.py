d530310c78845fde7a7dafa090f260960fb4ec58
OD Osama <bradenabc@gmail.com>
2021-04-12 11:33:10 -0400

    Create Source.py

---
+++ Source.py
@@ -0,0 +1,52 @@
+import time
+
+phrase = "I hate dirty Niggers, Hail Hitler"
+phrase2 = "I Cant Breathe!"
+phrase3 = "The End!"
+
+Character_name = "Johnquarius"
+nigga_age = "21"
+Nigga_color = "dark_black"
+best_race = "white guy"
+blue12 = "Police"
+color = "black"
+slur = "Niggers"
+
+Is_Retarded = True
+Is_Black = True
+
+print("The big black stinkin Niggers name was " + Character_name + ", ")
+time.sleep(2)
+print("The dumb ape " + Character_name + "had just turned " + nigga_age + ", ")
+time.sleep(1)
+print("the dumb nigger saw a white man say " + phrase.upper() + ". ")
+time.sleep(2)
+print("since he is black he chased after him,")
+time.sleep(2)
+print("he found the " + best_race + " hiding, ")
+time.sleep(2)
+print("but it was to late because the " + blue12 + " tackled his " + color + " ass to the ground,")
+time.sleep(2)
+print("the " + blue12 + "put a knee on the " + slur + " neck and he screamed,")
+print(phrase2)
+time.sleep(2)
+print("then he died and everyone was happy because everyone hates " + slur + "!")
+time.sleep(2)
+print(phrase3)
+
+times_to_repeat = 3
+
+time.sleep(5)
+print("get ready for racist spam")
+print("5")
+time.sleep(1)
+print("4")
+time.sleep(1)
+print("3")
+time.sleep(1)
+print("2")
+time.sleep(1)
+print("1")
+
+while times_to_repeat >= 0:
+    print(phrase)
